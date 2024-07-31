0c6609c3e6b15b561359055818e43e2a5b6890ae
Pablo Guasco <pablo.gusaco@naranjax.com>
2024-07-31 18:57:41 -0300

    Fix in Record model

--- src/models/Record.js
+++ src/models/Record.js
@@ -4,7 +4,7 @@ const recordSchema = new mongoose.Schema({
   operation_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Operation', required: true },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   amount: { type: String, required: true },
-  user_balance: { type: Number, required: true },
+  user_balance: { type: String, required: true },
   operation_response: { type: String, required: true },
   date: { type: Date, default: Date.now },
   deleted: { type: Boolean, default: false }
